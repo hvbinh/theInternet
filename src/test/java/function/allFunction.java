4b23aaec4ce0c3731f3c108803f9dec229672c6c
hvbinh <havanbinhk13t01@gmail.com>
2020-07-05 21:28:28 +0700

    commit1

--- src/test/java/function/allFunction.java
+++ src/test/java/function/allFunction.java
@@ -1,14 +1,17 @@
 package function;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.How;
+
+import java.util.List;
 
 import static common.commonFunction.*;
 
 public class allFunction {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) throws InterruptedException {
         openBrowsers("chrome");
         visit("https://the-internet.herokuapp.com");
         click(How.XPATH,"//a[.='A/B Testing']");
@@ -23,8 +26,31 @@ public class allFunction {
 
         click(How.XPATH,"//a[.='Basic Auth']");
         visit("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+        backPrevious();
+        backPrevious();
+
+        click(How.XPATH, "//a[.='Broken Images']");
+        for(WebElement e : imagelist(How.TAG_NAME, "img"))
+        {
+            isImageBroken(e);
+        }
+        backPrevious();
+        //waiting(2000);
+
+        click(How.XPATH,"//a[.='Checkboxes']");
+        click(How.XPATH,"//input[@type='checkbox'][1]");
+        click(How.XPATH,"//input[@type='checkbox'][2]");
+        backPrevious();
+
+        click(How.XPATH,"//a[.='Context Menu']");
+        rightClick(How.ID,"hot-spot");
+        navigateHomePage();
+
+
 
 
     }
+
+
 
 }
